fe9c89fc2535c0e0a4d016a92f10f4f846c964ac
Brian Martinez <100006298+bamart-dev@users.noreply.github.com>
2025-04-01 13:32:14 -0700

    Completed Wave 01 & test

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,22 @@
 class Vendor:
-    pass
+    # inventory attribute, can be passed in or left as default (default is
+    #   empty list)
+    # add() method - adds an item to inventory list and returns item added
+    # remove() method - remove an item from inventory list if the item is
+    #   present, else return False
+    def __init__(self,inventory = None):
+        if inventory is None:
+            self.inventory = []
+        else:
+            self.inventory = inventory
+
+    def add(self,item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self,item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
