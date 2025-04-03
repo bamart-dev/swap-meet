032932c6a2e08e487faf039471c8ef83adcdbb63
Brian Martinez <100006298+bamart-dev@users.noreply.github.com>
2025-04-03 15:19:32 -0700

    implemented get_by_category, get_best_by_category, and swap_best_by_category in Vendor class; completed wave_06 tests, ran all tests and tests passed; ran test_integration_wave_04_05_06, test passed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -43,16 +43,16 @@ class Vendor:
             return True
         else:
             return False
-        
+
     def swap_first_item(self, other_Vendor):
         # second way: if not self.inventory or not other_Vendor.inventory:
         if self.inventory == [] or other_Vendor.inventory == []:
             return False
-        
-        else: 
+
+        else:
             first_item_vendor = self.inventory[0]
             first_item_other_vendor = other_Vendor.inventory[0]
-            
+
             if first_item_vendor and first_item_other_vendor:
                 self.inventory.remove(first_item_vendor)
                 other_Vendor.inventory.append(first_item_vendor)
@@ -62,4 +62,61 @@ class Vendor:
 
                 return True
 
+    def get_by_category(self, category):
+        category_list = []
+        for item in self.inventory:
+            if item.get_category() == category:
+                category_list.append(item)
 
+        return category_list
+
+    def get_best_by_category(self, category):
+        best_by_category_list = []
+
+        for item in self.inventory:
+            if item.get_category() == category:
+                best_by_category_list.append(item)
+
+        if not best_by_category_list:
+            return None
+
+        best_condition = best_by_category_list[0].condition
+        best_condition_item = best_by_category_list[0]
+
+        for item in best_by_category_list:
+            if item.condition > best_condition:
+                best_condition = item.condition
+                best_condition_item = item
+
+        return best_condition_item
+
+    def swap_best_by_category(self, other_vendor, my_priority, their_priority):
+        # my_prio = Electronics
+        # their_prio = Clothing
+        # swap best Clothing from my inventory with the best Electronic from
+        #   their inventory
+        my_best_item = self.get_best_by_category(their_priority)
+        their_best_item = other_vendor.get_best_by_category(my_priority)
+
+        if not my_best_item or not their_best_item:
+            return False
+
+        self.inventory.remove(my_best_item)
+        self.inventory.append(their_best_item)
+        other_vendor.inventory.remove(their_best_item)
+        other_vendor.inventory.append(my_best_item)
+
+        return True
+
+
+
+
+
+
+
+
+
+
+            # if best_by_category_list:
+            #   compare here
+            # else:
